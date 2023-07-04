cdf984d9ba558acb82aa22029dfe7a46b00dd190
Athilakshmi-ms <athilakshmi.ms@gmail.com>
2023-07-04 16:47:08 +0200

    made some changes to the code, now the demo weather app is working

--- script.js
+++ script.js
@@ -1,45 +1,37 @@
-import Data from "./config.js";
+//import Data from "./config.js";
 const searchBar = document.querySelector('#searchBar');
 const container = document.querySelector(".container");
 const cityNameContainer = document.querySelector('.city-name');
-
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-console.log("hello world")
-// Event will start on a keyup action
-searchBar.addEventListener('keyup', (event) => {
-    // checking the action for specific key (Enter)
-    if (event.key === "Enter") {
-        // Store target in variable
-        const thisCity = event.target.value.toLowerCase();
-        const apiUrl = "https://api.openweathermap.org/data/2.5/forecast/?q=" + thisCity + "&appid=" + Data.key;
-        event.currentTarget.value = '';
-        // Fetching first api to get the City coordinates
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                const lon = data.city.coord.lon;
-                const lat = data.city.coord.lat;
 
-                cityNameContainer.innerHTML = data.city.name;
+searchBar.addEventListener('keyup', (event) => { // checking the action for specific key (Enter)
+    if (event.key === "Enter") {
+        const thisCity = event.target.value.toLowerCase(); // Store target in variable
+        event.currentTarget.value = '';
+        fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${thisCity}&appid=ead2bac0ebab43c42cf82b0aabc58885`)
+            .then(response => response.json())  // Fetching first api to get the City coordinates
+            .then(data => {
+                console.log(data)
+                const lon = data[0].lon;
+                const lat = data[0].lat;
+                cityNameContainer.innerHTML = data[0].name.toUpperCase();
 
                 // Fetching final data according to the coordinates
-                fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&cnt=5&units=metric&exclude=minutely,hourly,alerts&appid=" + Data.key)
-                    .then(response => response.json())
+                //fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&cnt=5&units=metric&exclude=minutely,hourly,alerts&appid=" + Data.key)
+                fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=5&units=metric&exclude=minutely,hourly,alerts&appid=ead2bac0ebab43c42cf82b0aabc58885`) 
+                .then(response => response.json())
                     .then(result => {
                         console.log('Welcome to this basic weather app. this is not a product but the product of an academic exercise.')
-
-                        // Removing all child elements from Container before creating new set of elements
-                        while (container.firstChild) {
-                            container.removeChild(container.firstChild);
+                        console.log(result)
+                        while (container.firstChild) {  // Removing all child elements from Container before creating new set of elements
+                            container.removeChild(container.firstChild);    
                         };
-
-                        // Looping through 5 days of weather data
-                        for (let i = 0; i < 5; i++) {
+                        for (let i = 0; i < 5; i++) {   // Looping through 5 days of weather data
 
                             // Use the remainder operator (%) to switch from saturday (last in array) back to sunday (first in array)
                             const date = new Date();
                             let dayOfTheWeek = weekdays[(date.getDay() + i) % 7];
-                            const data = result.daily[i];
+                            const data = result.list[i];
 
                             // Create the elements with Data
                             const card = document.createElement('div');
@@ -51,7 +43,7 @@ searchBar.addEventListener('keyup', (event) => {
                             card.appendChild(imageBox);
 
                             const cardImg = document.createElement('img');
-                            cardImg.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+                            cardImg.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
                             imageBox.appendChild(cardImg);
 
                             const contentBox = document.createElement("div");
@@ -76,7 +68,7 @@ searchBar.addEventListener('keyup', (event) => {
 
                             const currentTemp = document.createElement("span");
                             currentTemp.classList.add("current-temp");
-                            currentTemp.innerHTML = data.temp.day + "°C";
+                            currentTemp.innerHTML = data.main.temp + "°C";
                             currentTempBox.appendChild(currentTemp);
 
                             const minMaxTemperatures = document.createElement("div");
@@ -89,18 +81,17 @@ searchBar.addEventListener('keyup', (event) => {
 
                             const minTemp = document.createElement("span");
                             minTemp.classList.add("min-temp")
-                            minTemp.innerHTML = data.temp.min + "°C";
+                            minTemp.innerHTML = data.main.temp_min + "°C";
                             minMaxTemperatures.appendChild(minTemp);
 
                             const maxTemp = document.createElement("span");
                             maxTemp.classList.add("max-temp")
-                            maxTemp.innerHTML = data.temp.max + "°C";
+                            maxTemp.innerHTML = data.main.temp_max + "°C";
                             minMaxTemperatures.appendChild(maxTemp);
                         };
                     });
             })
             .catch((error) => {
-                // If there are errors, send out an error message
                 console.error('Error:', "not a place!");
                 while (container.firstChild) {
                     container.removeChild(container.firstChild);
